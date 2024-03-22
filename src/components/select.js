bb2d3f90036f5935218254a2028c917748735e5c
K <2322614@cognizant.com>
2024-03-22 10:06:24 +0530

    Eight commit

--- src/components/select.js
+++ src/components/select.js
@@ -5,19 +5,32 @@ function Select() {
   return (
     <div>
       <div className="contai">
+
           <div className="one">
               <div className="content">
                   <a href="/passport" className="bn">Passport</a>
               </div>
           </div>
+
           <div className="two">
               <div className="content">
                   <a href="/visa" className="bn">Visa</a>
               </div>
           </div>
+
+          <div className="three">
+              <div className="content">
+                  <a href="/renewpassport" className="bn">Passport Renewal</a>
+              </div>
+          </div>
+
+          <div className="four">
+              <div className="content">
+                  <a href="/cancelvisa" className="bn">Cancel Visa</a>
+              </div>
+          </div>
+          
       </div>
-
-
     </div>
   );
 }
